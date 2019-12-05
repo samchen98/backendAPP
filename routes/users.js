efa4dfd8c70058ead4fbadc47d5e47a4c6cae454
chen16s <chen16s@live.unc.edu>
2019-12-04 20:35:22 -0500

    added backend

---
+++ routes/users.js
@@ -0,0 +1,91 @@
+const router = require('express').Router();
+let User = require('../models/user.model.js');
+
+router.route('/').get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  console.log("more stuff");
+
+  const { body } = req;
+  let {
+    fname,
+    lname,
+    email,
+    password,
+    userType
+  } = body;
+  
+  console.log(email);
+  if (!email) {
+    return res.send({
+      success: false,
+      message: 'Error: Email cannot be blank.'
+    });
+  }
+  if (!password) {
+    return res.send({
+      success: false,
+      message: 'Error: Password cannot be blank.'
+    });
+  }
+
+  email = email.toLowerCase();
+  email = email.trim();
+
+  // Steps:
+  // 1. Verify email doesn't exist
+  // 2. Save
+  User.find({
+    email: email
+  }, (err, previousUsers) => {
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error: Server error'
+      });
+    } else if (previousUsers.length > 0) {
+      return res.send({
+        success: false,
+        message: 'Error: Account already exist.'
+      });
+    }
+
+    // Save the new user
+    const newUser = new User();
+    newUser.fname = fname;
+    newUser.lname = lname;
+    newUser.userType = userType;
+    newUser.email = email;
+    newUser.password = newUser.generateHash(password);
+    newUser.save((err, user) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      }
+      return res.send({
+        success: true,
+        message: 'Signed up'
+      });
+    });
+  });
+
+  // const username = req.body;
+  // console.log(username);
+
+  // const newUser = new User(username);
+
+  // newUser.save()
+  //   .then(() => res.json('User added!'))
+  //   .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 8080;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+module.exports.bcrypt = bcrypt;
+
+app.use(cors());
+app.use(bodyParser.json());
+const dbRoute = "mongodb+srv://sam:samchen1131@cluster0-xwslr.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+const usersRouter = require('./routes/users');
+
+
+app.use('/users', usersRouter);
+
+app.listen(process.env.PORT || 5000)
+
