19a5841dc32202c432778802cb8907e71cfbe0d8
chen16s <chen16s@live.unc.edu>
2019-12-08 17:35:49 -0500

    asdfa

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 
 const dbRoute = "mongodb+srv://sam:samchen1131@cluster0-xwslr.mongodb.net/test?retryWrites=true&w=majority";
