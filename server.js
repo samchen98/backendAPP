1061905bc4e888dc9ad165543e0041edf99f83a2
chen16s <chen16s@live.unc.edu>
2019-12-08 17:24:16 -0500

    asdfa

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const mongoose = require('mongoose');
 const PORT = 8080;
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 module.exports.bcrypt = bcrypt;
 
 app.use(cors());
