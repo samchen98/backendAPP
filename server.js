4fe5b16de372c6027b3f8e31b67100522c4f62aa
chen16s <chen16s@live.unc.edu>
2019-12-08 17:32:55 -0500

    asdfasdf

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ const mongoose = require('mongoose');
 const PORT = 8080;
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+
+
 module.exports.bcrypt = bcrypt;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+
+
 const dbRoute = "mongodb+srv://sam:samchen1131@cluster0-xwslr.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(dbRoute, { useNewUrlParser: true });
 const connection = mongoose.connection;
@@ -28,5 +29,9 @@ const usersRouter = require('./routes/users');
 
 app.use('/users', usersRouter);
 
-app.listen(8080)
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
 
+
+
+module.exports = app ;
